abc4b59b193df1c812f8dc4877a773e3f18966c6
zen <mobi2009ro@gmail.com>
2023-08-21 20:14:52 -0700

    list

--- update_schema.php
+++ update_schema.php
@@ -1,18 +1,92 @@
 <?php
+require_once 'config.php';
+
+// Define parameters
+$databasePattern = 'infawork_company%';
+$listDatabases = true; // Set to true to list databases
+$performUpdates = false; // Set to true to perform updates
+$targetDatabase = "infawork_company_71"; // Set to "ALL" to update all databases
+
 // Create connection
 $conn = new mysqli($servername, $username, $password);
 
-// Get a list of databases starting with 'infawork_company'
-$sql = "SHOW DATABASES LIKE 'infawork_company%'";
-$result = $conn->query($sql);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-if ($result) {
-    $databaseNames = $result->fetch_all();
-    foreach ($databaseNames as $database) {
-        echo $database[0] . "\n";
+if ($listDatabases) {
+    // List databases based on the provided pattern
+    $sql = "SHOW DATABASES LIKE '$databasePattern'";
+    $result = $conn->query($sql);
+
+    if ($result) {
+        $databaseNames = $result->fetch_all();
+        foreach ($databaseNames as $database) {
+            echo $database[0] . "\n";
+        }
+    } else {
+        echo "Query failed: " . $conn->error;
     }
-} else {
-    echo "Query failed: " . $conn->error;
+}
+
+if ($performUpdates && $targetDatabase === "ALL") {
+    // Get a list of databases based on the provided pattern
+    $sql = "SHOW DATABASES LIKE '$databasePattern'";
+    $result = $conn->query($sql);
+
+    if ($result) {
+        while ($row = $result->fetch_assoc()) {
+            $dbName = $row["Database"];
+
+            // Connect to the specific database
+            $dbConn = new mysqli($servername, $username, $password, $dbName);
+
+            if ($dbConn->connect_error) {
+                die("Connection failed: " . $dbConn->connect_error);
+            }
+
+            // Add a new field to the table 'infa_accounting_ap_automation'
+            $alterQuery = "ALTER TABLE infa_accounting_ap_automation ADD COLUMN new_field CHAR(1)";
+
+            if ($dbConn->query($alterQuery) === TRUE) {
+                echo "Added 'new_field' column to infa_accounting_ap_automation in $dbName\n";
+            } else {
+                echo "Error adding column: " . $dbConn->error;
+            }
+
+            $dbConn->close();
+        }
+    } else {
+        echo "Query failed: " . $conn->error;
+    }
+} elseif ($performUpdates && $targetDatabase !== "ALL") {
+    // Check if the target database exists
+    $sql = "SHOW DATABASES LIKE '$targetDatabase'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows === 1) {
+        $dbName = $targetDatabase;
+        // Connect to the specific database
+        $dbConn = new mysqli($servername, $username, $password, $dbName);
+
+        if ($dbConn->connect_error) {
+            die("Connection failed: " . $dbConn->connect_error);
+        }
+
+        // Add a new field to the table 'infa_accounting_ap_automation'
+        $alterQuery = "ALTER TABLE infa_accounting_ap_automation ADD COLUMN new_field CHAR(1)";
+
+        if ($dbConn->query($alterQuery) === TRUE) {
+            echo "Added 'new_field' column to infa_accounting_ap_automation in $dbName\n";
+        } else {
+            echo "Error adding column: " . $dbConn->error;
+        }
+
+        $dbConn->close();
+    } else {
+        echo "Database '$targetDatabase' not found.\n";
+    }
 }
 
 $conn->close();
